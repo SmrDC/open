83a121474de78a6f03d84b38151ea0e444f6b8d3
Smridh Thapar <smridh@gmail.com>
2015-06-21 01:58:03 -0700

    Initial Commit

---
+++ tv/tv.js
@@ -0,0 +1,90 @@
+var smrTVServerUrl = "http://smridh.com/tv/v1";
+var activeChannel;
+if(window.location.hash) {
+    activeChannel = window.location.hash.substr(2); // remove #/
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    startTV();
+});
+
+window.onhashchange = function() {
+    window.location.reload(true);
+};
+
+function startTV() {
+    if(activeChannel) {
+        $("#media-player").show();
+    } else {
+        loadChannelList();
+        $("#channel-list").show();
+        $(".fb-comments").show();
+    }
+}
+
+function loadChannelList() {
+    var channelListUrl = smrTVServerUrl + "/channel-list";
+    $.getJSON(channelListUrl)
+        .success(function(channelList) {
+            var channelListContent = "<ul>";
+            $.each(channelList, function(index, channel){
+                channelListContent += "<li><a href='#/" + channel.channelName + "'>"
+                    + channel.channelName + "</a></li>";
+            });
+            channelListContent += "</ul>";
+            $("#channel-list").html(channelListContent);
+        })
+        .fail(function (error) {
+            $("#channel-list").text("Unable to list the channels");
+        });
+}
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'media-player' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player("media-player", {
+        playerVars: {
+            controls: 0, // don't show video controls
+            disablekb: 1, // disable keyboard
+            iv_load_policy: 3, // annotations off
+            rel: 0, // don't show related videos at end
+            showinfo: 0 // don't show video title
+        },
+        events: {
+            'onReady': playNextTrack,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        playNextTrack();
+    }
+}
+
+function playNextTrack() {
+    if(!activeChannel) return;
+
+    var trackFeed = smrTVServerUrl + "/channel/" + activeChannel + "/now-playing";
+    $.getJSON(trackFeed)
+        .success(function (track) {
+            player.loadVideoById(track.video.id, track.startSeconds);
+            player.playVideo();
+            gaPushTrackPageview();
+        })
+        .fail(function (error) {
+            if (error.status == 503) {
+                setTimeout(function(){playNextTrack(player);}, 5000);
+            } else {
+                alert("Unable to play this channel");
+            }
+        });
+}
