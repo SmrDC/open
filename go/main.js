dd2d21ba7822f931d557bb4aa0b92f06c11c8fa8
smridh <smridh>
2020-07-28 20:48:55 -0700

    ✔️

--- go/main.js
+++ go/main.js
@@ -1,20 +1,20 @@
 // © smridh.com
 
-urlMap.get = function(key) {
+getUrl = function(key) {
   const urlMap = {
     smridh: "https://meet.google.com/jbi-qjsd-hzi",
     "smridh:": "https://meet.google.com/jbi-qjsd-hzi",
-    lightningMcQueen: "https://youtu.be/UCoogBnurWk",
   };
 
   return urlMap[key];
 };
 
 function go(id, passkey) {
-  const url = urlMap.get(id);
+  const url = getUrl(id);
   return location.replace(url ? url : "https://go.smridh.com/" + id);
 }
 
+const url = new URL(location);
 const urlFragment = url.hash;
 const tag = urlFragment ? urlFragment.substr(1) : url.searchParams.get("tag");
 if(tag)
